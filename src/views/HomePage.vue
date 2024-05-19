fa9abd6e056a430f77f1439a89e6ae9148b75bcc
nikita080428 <nikita080428@mail.ru>
2024-05-19 16:33:13 +0300

    HomePage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -9,7 +9,18 @@ export default {
 
 
 <template>
-    Страница 1
+    <div class="container d-flex justify-content-between" style="margin-top: 5%;">
+    <main >
+        <header>
+            <h1 class="border-bottom">Добро пожаловать</h1>
+        </header>
+        
+
+    </main>
+    <aside>
+        <h3>Доп меню</h3>
+    </aside>
+</div>
 </template>
 
 
