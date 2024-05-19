622b88f208c79d149923776c496234d04c32c248
nikita080428 <nikita080428@mail.ru>
2024-05-19 16:43:02 +0300

    Basic_form

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -10,10 +10,20 @@ export default {
 
 <template>
     <div class="container d-flex justify-content-between" style="margin-top: 5%;">
-    <main >
-        <header>
-            <h1 class="border-bottom">Добро пожаловать</h1>
+    <main style="width: 930px;" >
+        <header class="d-flex justify-content-center">
+            <h1 class="pb-3 border-bottom">Добро пожаловать</h1>
         </header>
+        <section class="container d-flex justify-content-center" style="margin-top: 5%; background-color: #F9F9F9;">
+            <div class="d-flex flex-column align-items-center">
+                <!-- Контент -->
+            <div style="margin-top: 5%;">
+                <h3>Обновление</h3>
+            </div>
+            <hr style="width: 930px;">
+
+        </div>
+        </section>
         
 
     </main>
